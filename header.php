52f07fa0c25caa4728b2989653ea768c4383bf42
emyx2077 <emily.saintetienne@gmail.com>
2022-03-03 11:44:50 +0100

    a

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><i class="bi bi-music-note-beamed"></i> jsp</a>
+            <a class="navbar-brand" href="index.php"><i class="bi bi-music-note-beamed"></i> Wrapped</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
@@ -11,16 +11,16 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
                     <li class="nav-item">
-                        <a class="nav-link" aria-current="page" href="index.php">Home</a>
+                        <a class="nav-link" aria-current="page" href="index.php">Accueil</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="donnees.php">Data</a>
+                        <a class="nav-link" href="listing.php">Liste</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="galerie.php">Galery</a>
+                        <a class="nav-link" href="form_recherche.php">Recherche</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="contact.php">Contact</a>
+                        <a class="nav-link" href="admin.php">Admin</a>
                     </li>
                 </ul>
             </div>
