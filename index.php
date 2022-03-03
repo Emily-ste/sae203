3af132f41f1821f1f20de1b98feea67313871c2c
emyx2077 <emily.saintetienne@gmail.com>
2022-03-03 12:17:33 +0100

    a

--- index.php
+++ index.php
@@ -1,35 +1,12 @@
 <?php
     require 'debut.php';
-    require 'header.php'
+    require 'header.php';
 ?>
 
 <main>
 
     <h1>blabla</h1>
 
-    <div id="main">
-        <div class="item" class="align">
-            <div class="hide" class="data" >
-                <p>
-                    images <br>
-                    titre <br>
-                    sortie <br>
-                    durée <br>
-                    style <br>
-                    nombre <br>
-                    label <br>
-                </p>
-            </div>
-        </div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-        <div class="item"></div>
-    </div>
 </main>
 
 <?php
--- listing.php
+++ listing.php
@@ -1,6 +1,9 @@
 <?php
 
+require 'debut.php';
+require 'header.php';
 
+/*
 $mabd = new PDO('mysql:host=localhost;dbname=sae203;charset=UTF8;', 'sae203', 'lasaecgenial');
 $mabd->query('SET NAMES utf8;');
 
@@ -11,6 +14,35 @@ foreach ($resultat as $value) {
     echo $value['titre_album'] . ' , ' . $value['release_album'];
     echo '<br> auteur: ' . $value['nom_artist']. '<hr>';
 }
+*/
 
 ?>
 
+<div id="main">
+    <div class="item">
+        <div class="test">
+            <p class="hide data">
+                titre <br>
+                sortie <br>
+                durée <br>
+                style <br>
+                nombre <br>
+            </p>
+        </div>
+    </div>
+
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+    <div class="item"></div>
+</div>
+
+
+<?php
+require 'footer.php';
+require 'fin.php';
+?>
