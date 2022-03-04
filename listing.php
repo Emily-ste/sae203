603966c27dad0a2f47fc4a634fb0d03a9286361a
emyx2077 <emily.saintetienne@gmail.com>
2022-03-04 11:15:55 +0100

    a

--- listing.php
+++ listing.php
@@ -20,7 +20,7 @@ $resultat = $mabd->query($req);
     echo '<div id="main">';
         foreach ($resultat as $value) {
             echo '<div class="item">';
-            echo '<img src="img/cover/'.str_replace(' ', '',$value['titre_album'].'.jpeg'.'" alt="'.$value['titre_album']).'">';
+            echo '<img class="img" src="img/cover/'.str_replace(' ', '',$value['titre_album'].'.jpeg'.'" alt="'.$value['titre_album']).'">';
             echo '<div class="divP">';
             echo '<p class="hide data">';
             echo ucwords(strtolower($value['titre_album'])).'<br>'.
