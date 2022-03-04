cd2f2b8e3b18cdc7b965c04a56bbb92238efd8e7
emyx2077 <emily.saintetienne@gmail.com>
2022-03-04 10:00:07 +0100

    a

--- listing.php
+++ listing.php
@@ -3,14 +3,14 @@
 require 'debut.php';
 require 'header.php';
 
-/*
+
 $mabd = new PDO('mysql:host=localhost;dbname=sae203;charset=UTF8;', 'sae203', 'lasaecgenial');
 $mabd->query('SET NAMES utf8;');
 
 $req = "SELECT * FROM albums INNER JOIN artists ON albums.id_artist = artists.id_artist";
 $resultat = $mabd->query($req);
 
-foreach ($resultat as $value) {
+/* foreach ($resultat as $value) {
     echo $value['titre_album'] . ' , ' . $value['release_album'];
     echo '<br> auteur: ' . $value['nom_artist']. '<hr>';
 }
