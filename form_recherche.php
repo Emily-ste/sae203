51c26d4d75e00600b6aeef197b5d7c009d279044
emyx2077 <emily.saintetienne@gmail.com>
2022-03-04 13:47:36 +0100

    a

--- form_recherche.php
+++ form_recherche.php
@@ -1,17 +1,18 @@
 <?php
-
 require 'debut.php';
 require 'header.php';
 ?>
 
+<script src="https://parsleyjs.org/dist/parsley.min.js"></script>
+<link rel="stylesheet" href="https://parsleyjs.org/src/parsley.css">
+
 <body>
     <div id="searchTile">
-        votre recherche:
-
         <form method="GET" action="reponse_recherche.php" data-parsley-validate>
-            prix min : <input type="text" name="prix" data-parsley-type="number"> <br>
-            prix max : <input type="text" name="prix" data-parsley-type="number"> <br>
-        <input type="submit" name="valider">
+            votre recherche: <br>
+            <input type="text" name="prix" data-parsley-type="number"> <br>
+            <input type="text" name="prix" data-parsley-type="number"> <br>
+                        <input type="submit" name="valider">
         </form>
     </div>
 </body>
