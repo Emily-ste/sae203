44eb2be01be0516a06f5923f73c0fc01b411ce80
emyx2077 <emily.saintetienne@gmail.com>
2022-03-22 12:21:44 +0100

    idk

--- lib/lib_crud.inc.php
+++ lib/lib_crud.inc.php
@@ -7,14 +7,14 @@ function connexion(){
         $mabd = new PDO('mysql:host=127.0.0.1;dbname=sae203;charset=UTF8;', USER, PASSWORD);
         $mabd->query('SET NAMES utf8;');
     } catch (PDOException $e) {
-        //print "Erreur : vps " . $e->getMessage() . '<br />';
+        //print "Erreur co: vps " . $e->getMessage() . '<br />';
     }
 
     try {
         $mabd = new PDO('mysql:host=localhost;dbname=sae203;charset=UTF8;', 'root', '');
         $mabd->query('SET NAMES utf8;');
     }  catch(Exception $r) {
-        //print "Erreur : localhost " . $r->getMessage() . '<br />';
+        //print "Erreur co: localhost " . $r->getMessage() . '<br />';
     }
 
     return $mabd;
