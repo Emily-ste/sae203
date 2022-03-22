17b742e36e114982bf825beb6d77aac305d7af80
emyx2077 <emily.saintetienne@gmail.com>
2022-03-22 19:21:49 +0100

    fix-img-edit

--- admin/table1_update_valide.php
+++ admin/table1_update_valide.php
@@ -4,7 +4,7 @@
 <body>
 <a href="admin.php">retour au tableau de bord</a>
 
-<p>vous venez de modifier un album</p>
+<p>Validation de la modification...</p>
 
 
 <?php
@@ -12,6 +12,8 @@
 require '../lib/lib_crud.inc.php';
 $mabd = connexion();
 
+
+///get some variable from table1_update
 $titre=$_POST['titre'];
 $sortie=$_POST['sortie'];
 $duree=$_POST['duree'];
@@ -21,8 +23,27 @@ $artist=$_POST['artist'];
 
 $num=$_POST['num'];
 
-//var_dump($_FILES);
 
+
+//on renomme l'image avec le nom mis a jour de l'album
+$ask = 'SELECT titre_album FROM  albums WHERE id_album ='.$num;
+    try {
+        $result = $mabd->query($ask);
+    } catch (PDOException $e) {
+        // s'il y a une erreur, on l'affiche
+        echo '<p>Erreur : ' . $e->getMessage() . '</p>';
+        die();
+    }
+    $path = $result->fetch();
+
+    $path = '../img/cover/'.str_replace(' ', '',$path['titre_album']).'.jpeg';
+
+    //si le fichier existe on le renomme
+    if (file_exists($path)) {
+        rename ($path, '../img/cover/'.$titre.'.jpeg');
+    }
+
+//on verifie si une nouvelle image a été uploader lors de la modification, si oui on check le format
 
 if(!empty($_FILES['pic']['name'])){
     $imageType=$_FILES["pic"]["type"];
@@ -34,8 +55,14 @@ if(!empty($_FILES['pic']['name'])){
         die();
     }
 
+    //on check l'extension du fichier uploader, si c'est un jpg on modifie en jpeg
     $ext = str_replace('image/', '',$imageType);
-    echo $ext;
+
+    if($ext == 'jpg'){
+        $ext = 'jpeg';
+    }
+
+    //on sauvegarde l'image
     $nomimg = str_replace(' ', '',$titre);
 
     if(is_uploaded_file($_FILES["pic"]["tmp_name"])) {
@@ -47,10 +74,31 @@ if(!empty($_FILES['pic']['name'])){
         echo '<p>Problème : image non chargée...</p>'."\n";
         die();
     }
+
+    //on regarde si l'image est en png, si oui on l'dit en jpeg
+    if($ext == "png"){
+        $filePath = '../img/cover/'.$nomimg.'.png';
+        $filePathEdit = '../img/cover/'.$nomimg;
+
+        $image = imagecreatefrompng($filePath);
+        $bg = imagecreatetruecolor(imagesx($image), imagesy($image));
+        imagefill($bg, 0, 0, imagecolorallocate($bg, 255, 255, 255));
+        imagealphablending($bg, TRUE);
+        imagecopy($bg, $image, 0, 0, 0, 0, imagesx($image), imagesy($image));
+        imagedestroy($image);
+        $quality = 50; // 0 = worst / smaller file, 100 = better / bigger file
+        imagejpeg($bg, $filePathEdit . ".jpeg", $quality);
+        imagedestroy($bg);
+
+        //on delete l'ancien png
+        if(file_exists($filePath)){unlink($filePath);}
+    }
 }
-imagejpeg("../img/cover/".$nomimg.'.'.$ext);
 
 
+
+
+//on maj la bdd avec les nouvelles valeurs
 $req = "UPDATE albums SET  titre_album='".$titre."', 
                             release_album='".$sortie."', 
                             lenght_album='".$duree."', 
@@ -60,6 +108,7 @@ $req = "UPDATE albums SET  titre_album='".$titre."',
         WHERE id_album='$num';";
 
 
+//on try d'envoyer la requete
 try {
     $resultat = $mabd->query($req);
 } catch (PDOException $e) {
