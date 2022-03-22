7b03f29e4352cef6f2f395dd3667adfa51ee76f8
emyx2077 <emily.saintetienne@gmail.com>
2022-03-22 08:39:38 +0100

    menulink

--- header.php
+++ header.php
@@ -2,7 +2,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-dark">
         <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><i class="bi bi-music-note-beamed"></i> Wrapped</a>
+            <a class="navbar-brand" href="/"><i class="bi bi-music-note-beamed"></i> Wrapped</a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
